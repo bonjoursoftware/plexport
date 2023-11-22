ebbe3c28d5fa85665bd26eb0b5c586d64d8e1c98
Antoine <achatain@users.noreply.github.com>
2023-11-22 16:58:20 +0000

    Import Plex films into Bonjour Software MyCollections

--- plexport.sh
+++ plexport.sh
@@ -5,5 +5,11 @@ docker run \
   --rm \
   --network=host \
   --env PLEX_API_TOKEN="$PLEX_API_TOKEN" \
+  --env PLEX_HOST="$PLEX_HOST" \
+  --env MONGO_HOST="$MONGO_HOST" \
+  --env MONGO_USER="$MONGO_USER" \
+  --env MONGO_PASSWORD="$MONGO_PASSWORD" \
+  --env MONGO_DATABASE="$MONGO_DATABASE" \
+  --env MONGO_COLLECTION="$MONGO_COLLECTION" \
   bonjoursoftware/plexport:local \
-  --host "$PLEX_HOST"
+  --load
